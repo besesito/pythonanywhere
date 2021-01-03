cbb95801b5ba8642a984b06bf20e9827dcb2f863
besesito <bartekszmude@gmail.com>
2021-01-04 00:36:20 +0100

    Added count app

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,9 +18,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
+import countword.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    path('count/', countword.views.count, name='count'),
+    path('count/result', countword.views.counted, name='counted'),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
